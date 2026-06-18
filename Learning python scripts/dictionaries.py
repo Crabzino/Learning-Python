fd20499537a8ec3d64c5d13cd1767ae51d6f87e1
Crabzino <adamilebbusiness@gmail.com>
2026-06-18 16:18:34 +0100

    added more to dictionaries script

--- Learning python scripts/dictionaries.py
+++ Learning python scripts/dictionaries.py
@@ -107,3 +107,162 @@ plays["Respect"] += 5  #this adds an additional 5 plays to the key
 
 library = {"The Best Songs": plays, "Sunday Feelings": {}}  #this outputs the value of the best songs to be the dictionary called plays. sunday feelings has an empty value
 #print(library)
+
+
+
+#we can obtain a value from dictionary by printing the key
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+#print(zodiac_elements["earth"])
+#print(zodiac_elements["fire"])
+
+
+
+
+#invalid keys:  IF WE TRIED TO PRINT A VALUE BY USING A KEY THAT DOES NOT EXIST, IT WILL OUTPUT AN ERROR. TO AVOID THAT USE AN IF STATEMENT
+zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "Leo", "Sagittarius"], "earth": ["Taurus", "Virgo", "Capricorn"], "air":["Gemini", "Libra", "Aquarius"]}
+
+zodiac_elements["energy"] = "Not a Zodiac element"   #it will work now because we added the key
+
+#if "energy" in zodiac_elements:      #it will only output the print statement if the key exists
+  #print(zodiac_elements["energy"])
+
+
+
+
+
+#WE CAN USE THE .get() METHOD TO GET A KEY'S VALUE. IF THE KEY DOES NOT EXIST IT WILL OUTPUT none. OR YOU CAN ASSIGN WHAT IT SHOULD OUTPUT IF IT DOES NOT EXIST
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+
+
+tc_id = user_ids.get("teraCoder", 100000) #this will output the value of the key as the key exists. the 100000 that is after the key is what would be outputed if the key does not exists
+#print(tc_id)
+
+stack_id = user_ids.get("superStackSmash", 100000)  #the output will be 100000 as the key does not exist
+#print(stack_id)
+
+
+
+
+
+
+building_heights = {"Burj Khalifa": 828, "Shanghai Tower": 632, "Abraj Al Bait": 601, "Ping An": 599, "Lotte World Tower": 554.5, "One World Trade": 541.3}
+
+#this line will return 632:
+building_heights.get("Shanghai Tower")
+
+#this line will return None:
+building_heights.get("My House")
+
+
+
+#print(building_heights.get('Shanghai Tower', 0)) # Prints 632
+#print(building_heights.get('Mt Olympus', 0)) # Prints 0
+#print(building_heights.get('Kilimanjaro', 'No Value')) # Prints 'No Value'
+
+
+
+
+#WE CAN USE THE .pop() METHOD TO REMOVE KEYS FROM THE DICTIONARIES
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+health_points += available_items.pop("stamina grains", 0)  #if the key exists, it adds the value of the key to the health_points number and then removes. if it does not exist it adds 0 which is the number after the key in .pop()
+#print(health_points)
+#print(available_items)
+
+health_points += available_items.pop("power stew", 0)  #same here
+health_points += available_items.pop("mystic bread", 0)   #mystic bread does not exist, so it added 0 to health_points
+
+#print(available_items, health_points)
+
+
+
+
+#HOW TO GET ALL KEYS WITHOUT VALUES   .keys()
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+#print(list(user_ids))   #using list, this will generate a list of the keys without the values
+
+#for user in user_ids.keys():    #this will generate a dict_keys view of the keys in the dictionary
+    #print(user)
+
+
+
+#test_scores = {"Grace":[80, 72, 90], "Jeffrey":[88, 68, 81], "Sylvia":[80, 82, 84], "Pedro":[98, 96, 95], "Martin":[78, 80, 78], "Dina":[64, 60, 75]}
+#for student in test_scores.keys():
+ #print(student)
+
+
+
+
+
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+
+users = user_ids.keys()      #prints all keys
+lessons = num_exercises.keys()  
+
+#print(users)
+#print(lessons)
+
+
+
+
+
+#HOW TO GET ALL VALUES WITHOUT KEYS       .values()
+test_scores = {"Grace":[80, 72, 90], "Jeffrey":[88, 68, 81], "Sylvia":[80, 82, 84], "Pedro":[98, 96, 95], "Martin":[78, 80, 78], "Dina":[64, 60, 75]}
+
+#for score_list in test_scores.values():
+ #print(score_list)   #outputs all values
+
+print(list(test_scores.values()))  #outputs all values as a list
+
+
+
+#this adds all values of each key to the total_exercises number 
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+for num in num_exercises.values():
+  total_exercises += num
+
+#print(total_exercises)
+
+
+
+
+
+#HOW TO GET BOTH KEYS AND VALUES     .items()
+biggest_brands = {"Apple": 184, "Google": 141.7, "Microsoft": 80, "Coca-Cola": 69.7, "Amazon": 64.8}
+
+#for company, value in biggest_brands.items():   #using the .item(), it is iterating through each key and value
+   #print(f"{company} has a whopping value of {value} billion dollars")   #this will output a strings and for each, it contains company and value assigned to each
+
+
+
+#another example
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+
+#for occupation, percent in pct_women_in_occupation.items():
+  #print(f"Women make up {percent} percent of {occupation}s.")
+
+
+
+
+
+
+#REVIEW
+tarot = { 1: "The Magician", 2: "The High Priestess", 3: "The Empress", 4: "The Emperor", 5: "The Hierophant", 6: "The Lovers", 7: "The Chariot", 8: "Strength", 9: "The Hermit", 10: "Wheel of Fortune", 11: "Justice", 12: "The Hanged Man", 13: "Death", 14: "Temperance", 15: "The Devil", 16: "The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20: "Judgement", 21: "The World", 22: "The Fool"}
+
+spread = {}
+
+spread["past"] = tarot.pop(13)   #this removes the key and its value from the above list and adds the value here to the new assigned key
+spread["present"] = tarot.pop(22)   #output: {"present": "The Fool"}
+spread["future"] = tarot.pop(10)
+
+#for key, value in spread.items():
+  #print(f"Your {key} is the {value} card.")       # Your present is the The Fool card.

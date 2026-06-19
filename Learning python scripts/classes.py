7eb8fd425ca09a13f091460ab3daaa0e4ecccd76
Crabzino <adamilebbusiness@gmail.com>
2026-06-19 23:20:09 +0100

    learning classes begins

---
+++ Learning python scripts/classes.py
@@ -0,0 +1,13 @@
+#Learning Classes
+
+#Classes are a template for data types. it tells us the kind of information that they will hold and how programmers interact with them:
+class Facade:
+  pass       #we used pass here so the terminal does not cause an indent error
+
+
+
+#we got to create an instance of the class, similar to calling a function
+class Facade: 
+  pass
+
+facade_1 = Facade()   #we assign the class with brackets to a variable

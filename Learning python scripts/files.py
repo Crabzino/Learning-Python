1b934acd51bdda051f5d98dc5305ad8ec63e398d
Crabzino <adamilebbusiness@gmail.com>
2026-06-19 11:38:07 +0100

    added new Lines

---
+++ Learning python scripts/files.py
@@ -0,0 +1,7 @@
+#We can use, interact and open file using python. using the syntax: with open("file") as new_name:
+#                                                                     variable_name = new_name.read()
+
+#Note:this wont work here as we do not have a file called welcome.tx, thisis only for example purposes
+#with open("welcome.txt") as text_file:
+  #text_data = text_file.read()
+  #print(text_data)

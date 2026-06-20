7a1fbb0b455cf7bfe25af5ae1e6bd217b46d9ff8
Crabzino <adamilebbusiness@gmail.com>
2026-06-20 23:04:37 +0100

    New mini project with classes(oop)

---
+++ Python projects/class_mini_project.py
@@ -0,0 +1,44 @@
+class Menu:
+  def __init__(self, name, items, start_time, end_time):
+    self.name = name
+    self.items = items
+    self.start_time = start_time
+    self.end_time = end_time
+    
+  def __repr__(self):
+    return f"{self.name} menu is available from {self.start_time} to {self.end_time}"
+
+  def calculate_bill(self, purchased_items):
+    total_price = 0
+    for item in self.items.keys():
+        if item in purchased_items:
+            total_price += self.items[item]
+    return f"The total price is ${total_price}."
+    
+
+brunch_items = {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50}
+
+brunch_menu = Menu("brunch", brunch_items, 1100, 1600)
+
+
+
+early_bird_items = {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,}
+
+early_bird_menu = Menu("earley bird", early_bird_items, 1500, 1800)
+
+
+
+dinner_items = {'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,}
+
+dinner_menu = Menu("dinner", dinner_items, 1700, 2300)
+
+
+
+kids_items = {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}
+
+kids_menu = Menu("kids", kids_items, 1100, 2100)
+
+
+print(brunch_menu)
+print(brunch_menu.calculate_bill(["pancakes", "home fries", "coffee"]))
+print(early_bird_menu.calculate_bill(["salumeria plate", "mushroom ravioli (vegan)"]))

338bf18fafadbc380b9ee1bdbded9ad92c89c05e
Crabzino <adamilebbusiness@gmail.com>
2026-06-20 23:57:52 +0100

    Finished the classes mini project: A restaurant!

--- Python projects/class_mini_project.py
+++ Python projects/class_mini_project.py
@@ -1,7 +1,7 @@
 class Menu:
   def __init__(self, name, items, start_time, end_time):
     self.name = name
-    self.items = items
+    self.items = items  #a list
     self.start_time = start_time
     self.end_time = end_time
     
@@ -42,3 +42,61 @@ kids_menu = Menu("kids", kids_items, 1100, 2100)
 print(brunch_menu)
 print(brunch_menu.calculate_bill(["pancakes", "home fries", "coffee"]))
 print(early_bird_menu.calculate_bill(["salumeria plate", "mushroom ravioli (vegan)"]))
+
+
+
+class Franchise:
+  def __init__(self, address, menus):
+    self.address = address
+    self.menus = menus  # list
+
+  def __repr__(self):
+    return f"The address of the restaurant is {self.address}."
+
+  def available_menus(self, time):
+    available_menu = []
+    for menu in self.menus:
+      if time >= menu.start_time and time <= menu.end_time:
+        available_menu.append(menu)
+    return available_menu
+
+
+
+
+flagship_store_address = "1232 West End Road"
+new_installment_address = "12 East Mulberry Street"
+franchise_menus = [brunch_menu, early_bird_menu, dinner_menu, kids_menu]
+
+flagship_store = Franchise(flagship_store_address, franchise_menus)
+new_installment = Franchise(new_installment_address, franchise_menus)
+
+#print(new_installment)
+print(flagship_store.available_menus(1200))
+print(new_installment.available_menus(2100))
+
+
+
+
+class Business:
+  def __init__(self, name, franchises):
+    self.name = name
+    self.franchises = franchises  #a list
+    
+
+business1_name = "Basta Fazoolin' with my Heart"
+business1_franchises = [flagship_store, new_installment] 
+business1 = Business(business1_name, business1_franchises)
+
+
+take_a_arepa_items = {'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50}
+arepas_menu = Menu("Take a' Arepa", take_a_arepa_items, 1000, 2000)
+
+
+arepas_place_location = "189 Fitzgerald Avenue"
+arepas_place_menus = [arepas_menu]
+arepas_place = Franchise(arepas_place_location, arepas_menu)
+
+
+business2_name = "Take a' Arepa"
+business2_franchises = [arepas_place]
+business2 = Business(business2_name, business2_franchises)
